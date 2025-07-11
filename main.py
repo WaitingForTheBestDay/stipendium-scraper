2d9ab6df49b9ccf699f6172ec0952f6e62e5381e
Mykhailo <firemanplay66@gmail.com>
2025-07-11 13:58:35 +0300

    refactor error handling and improve request timeout management

--- main.py
+++ main.py
@@ -24,12 +24,26 @@ def on_not_found():
     }
     requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", params = pr)
 
+def on_error(e):
+    pr = {
+        'chat_id': CHAT_ID,
+        'text': f'Помилка: {e}',
+    }
+    requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", params=pr)
+
 if __name__ == "__main__":
-    r = requests.get(URL)
+    try:
+        r = requests.get(URL, timeout=10)
+        r.raise_for_status()  # Raise an error for HTTP errors
+    except requests.RequestException as e:
+        on_error(e)
+        raise ValueError("Network error or invalid response") from e
+
     soup = BeautifulSoup(r.text, 'html.parser')
 
     news = soup.find('div', class_='views-view-grid horizontal cols-3 clearfix')
     if not (news and isinstance(news, Tag)):
+        on_error("No news found on the page")
         raise ValueError("No news found on the page")
     titles = [t.text for t in news.find_all('h3')]
 
